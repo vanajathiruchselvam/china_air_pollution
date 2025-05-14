99fb1d42c5a7e05ba59c1567fde2e813a42e07be
Vanaja Thiruchselvam <34267506+vanajathiruchselvam@users.noreply.github.com>
2025-05-14 16:02:40 +0100

    Update multiapp.py

--- air_pollution_app/multiapp.py
+++ air_pollution_app/multiapp.py
@@ -1,1 +1,23 @@
+# multiapp.py
+import streamlit as st
+
+class MultiApp:
+    def __init__(self):
+        self.apps = []
+
+    def add_app(self, title, func):
+        self.apps.append({
+            "title": title,
+            "function": func
+        })
+
+    def run(self):
+        # Sidebar selectbox (not radio)
+        app_titles = [app["title"] for app in self.apps]
+        selected_app = st.sidebar.selectbox("Navigation", app_titles)
+
+        # Run the selected app
+        for app in self.apps:
+            if app["title"] == selected_app:
+                app["function"]()
 
