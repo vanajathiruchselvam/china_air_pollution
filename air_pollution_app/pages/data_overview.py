c3282f8af99ad0c684d4c149416450a4b990589e
Vanaja Thiruchselvam <34267506+vanajathiruchselvam@users.noreply.github.com>
2025-05-14 13:08:08 +0100

    Update data_overview.py

--- air_pollution_app/pages/data_overview.py
+++ air_pollution_app/pages/data_overview.py
@@ -3,8 +3,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Set the page config as the very first Streamlit command
-st.set_page_config(layout="wide")
+
 
 # Set background image using custom HTML
 def set_background():
